f1394f3a51d88412c588bfa5516733defca27b31
Lin Yang <liny@outlook.com>
2018-10-11 11:53:40 +1100

    ACG20: add more unit test cases

--- code/test/acg20.js
+++ code/test/acg20.js
@@ -3,21 +3,42 @@ const expectThrow = require("../helpers/expectThrow.js")
 var ACG20TOKEN = artifacts.require("ACG20");
 
 contract('API Support: add_new_user()', function(accounts) {
+  let acg20Inst;
+  let userInitBalance = 1e4;
+  let totalSupply = userInitBalance * accounts.length;
+  let userBalance = [];
+  let contractOwner = accounts[0];
   
-  it("Add two new user with different initial balance", async function() {
-    let acg20Inst = await ACG20TOKEN.deployed();
-    let user0 = accounts[0];
-    let user1 = accounts[1];
+  before("Add users with initial balances", async () => {
+    acg20Inst = await ACG20TOKEN.deployed();
 
-    user0_mint = acg20Inst.mint(user0, 100);
-    user1_mint = acg20Inst.mint(user1, 200);
+    accounts.forEach(async (user) => {
+      await acg20Inst.mint(user, userInitBalance);
+    });
+    accounts.forEach( (user) => {
+      userBalance[user] = userInitBalance;
+    });
+  });
+  it("Check the total balance is set correctly", async function() {
+    let totalSupplyValue = await acg20Inst.totalSupply.call();
+    assert.equal(totalSupplyValue.toNumber(), totalSupply, "Total supply should be 300")
+  });
+  it("Only contract owner is permitted to mint", async function () {
+    let mintAmount = 5e3;
+    await expectThrow(acg20Inst.mint(accounts[1], mintAmount, {from: accounts[1]}), "General user is not permitted to mint");
+  });
+  it("Contract owner could mint to incrase total supply", async function() {
+    let mintAmount = 5e3;
+    await acg20Inst.mint(accounts[1], mintAmount, {from: contractOwner});
+    totalSupply += mintAmount;
+    userBalance[accounts[1]] += mintAmount;
 
-    await user0_mint;
-    await user1_mint;
+    let newBalance = await acg20Inst.balanceOf.call(accounts[1]);
+    let newTotalSupply = await acg20Inst.totalSupply.call();
 
-    let totalSupply = await acg20Inst.totalSupply.call();
-    assert.equal(totalSupply.toNumber(), 300, "Total supply should be 300")
-  })
+    assert.equal(newBalance.toNumber(), userBalance[accounts[1]], "User's balance increased after minting");
+    assert.equal(newTotalSupply.toNumber(), totalSupply, "Total supply increased after minting");
+  });
 });
 
 contract('API Support: post_new_artwork()', function(accounts) {
@@ -27,17 +48,22 @@ contract('API Support: post_new_artwork()', function(accounts) {
 contract('API Support: buy_token()', function(accounts) {
   let acg20Inst;
   let contractOwner = accounts[0];
+  let totalSupply = 0;
 
-  it("Initial total supply should be zero", async () => {
+  before(async () => {
     acg20Inst = await ACG20TOKEN.deployed();
-
-    let totalSupply = await acg20Inst.totalSupply.call();
-    assert.equal(totalSupply.toNumber(), 0, "Initial otal supply should be 0")
+  });
+  it("Initial total supply should be zero", async () => {
+    let totalSupplyValue = await acg20Inst.totalSupply.call();
+    assert.equal(totalSupplyValue.toNumber(), totalSupply, "Intial total supply should be zero");
   });
   it("Buy token should increase total supply", async () => {
-    await acg20Inst.mint(accounts[1], 100, {from: contractOwner});
-    let totalSupply = await acg20Inst.totalSupply.call();
-    assert.equal(totalSupply.toNumber(), 100, "Total supply increased to 100")
+    let newTokenAmount = 1e5;
+    totalSupply += newTokenAmount;
+    await acg20Inst.mint(accounts[1], newTokenAmount, {from: contractOwner});
+
+    let totalSupplyValue = await acg20Inst.totalSupply.call();
+    assert.equal(totalSupplyValue.toNumber(), totalSupply, "Total supply increased")
   });
 });
 
@@ -47,7 +73,7 @@ contract('API Support: buy_artwork()', function(accounts) {
   let totalSupply = userInitBalance * accounts.length;
   let userBalance = [];
 
-  before("Add two users with initial balances", async () => {
+  before("Add users with initial balances", async () => {
     acg20Inst = await ACG20TOKEN.deployed();
 
     accounts.forEach(async (user) => {
@@ -121,91 +147,116 @@ contract('API Support: buy_artwork()', function(accounts) {
 contract('API Support: freeze_token()', function(accounts) {
 
   let acg20Inst;
-  let admin;
+  let admin = accounts[0];
   let userInitBalance = 1e4;
+  let userBalance = [];
+  let totalSupply;
+
+  let bidForArt1 = 0;
+  let bidderForArt1;
+
+  let bidForArt2 = 0;
+  let bidderForArt2;
+
+  let artist = accounts[4];
   let artwork1 = 0;
   let artwork2 = 1;
 
-  before ( async () => {
+  before(async () => {
     acg20Inst = await ACG20TOKEN.deployed();
 
-    admin = accounts[0];
-    await acg20Inst.mint(admin, userInitBalance);
-    await acg20Inst.mint(accounts[1], userInitBalance);
-    await acg20Inst.mint(accounts[2], userInitBalance);
-    await acg20Inst.mint(accounts[3], userInitBalance);
-    await acg20Inst.mint(accounts[4], userInitBalance);
+    accounts.forEach(async (user) => {
+      await acg20Inst.mint(user, userInitBalance);
+    });
+    accounts.forEach( (user) => {
+      userBalance[user] = userInitBalance;
+    });
+    totalSupply = userInitBalance * accounts.length;
+  });
+  it("Only contract owner is able of freezing tokens", async () => {
+    let frozenAmount = 1e3;
+    await expectThrow(acg20Inst.freeze(accounts[1], frozenAmount, artwork1, {from: accounts[1]}), "General user is not permitted to freeze tokens");
+  });
+  it("Frozen tokens amount should not exceed user's balance", async () => {
+    let frozenUser = accounts[1];
+    let frozenAmount = userBalance[frozenUser] + 1;
+    await expectThrow(acg20Inst.freeze(frozenUser, frozenAmount, artwork1, {from: admin}), "Frozen amount should not exceed user balance");
   });
   it("freeze() will reduce user balance but won't change total supply", async () => {
-    let frozen_amount = 1e3;
-    await acg20Inst.freeze(accounts[1], 1e3, artwork1, {from: admin});
+    bidForArt1 = 1e3;
+    bidderForArt1 = accounts[1];
+    await acg20Inst.freeze(bidderForArt1, bidForArt1, artwork1, {from: admin});
+    userBalance[bidderForArt1] -= bidForArt1;
 
-    let userBalance = await acg20Inst.balanceOf.call(accounts[1]);
-    assert.equal(userBalance.toNumber(), userInitBalance-frozen_amount, "User's balance should be reduced by frozen amount");
+    let newBalance = await acg20Inst.balanceOf.call(bidderForArt1);
+    assert.equal(newBalance.toNumber(), userBalance[bidderForArt1], "User's balance should be reduced by frozen amount");
 
-    let totalSupply = await acg20Inst.totalSupply.call();
-    assert.equal(totalSupply.toNumber(), userInitBalance*5, "Total supply should not affected by freeze() operation");
+    let newTotal = await acg20Inst.totalSupply.call();
+    assert.equal(newTotal.toNumber(), totalSupply, "Total supply should not affected by freeze() operation");
   });
   it("If other user provides a higher bid, then token is unfrozen for previous bidder", async () => {
-    let higher_frozen_amount = 2e3;
-    await acg20Inst.freeze(accounts[2], higher_frozen_amount, artwork1, {from: admin});
+    let prevBidForArt1 = bidForArt1;
+    let prevBidderForArt1 =bidderForArt1;
+    bidForArt1 = 2e3;
+    bidderForArt1 = accounts[2];
 
-    let user1Balance = await acg20Inst.balanceOf.call(accounts[1]);
-    assert.equal(user1Balance.toNumber(), userInitBalance, "Frozen tokens should be withdrawn");
+    await acg20Inst.freeze(bidderForArt1, bidForArt1, artwork1, {from: admin});
+    userBalance[prevBidderForArt1] += prevBidForArt1;
+    userBalance[bidderForArt1] -= bidForArt1;
+
+    let newBalance = await acg20Inst.balanceOf.call(prevBidderForArt1);
+    assert.equal(newBalance.toNumber(), newBalance, "Frozen tokens should be withdrawn");
   });
   it("User can even provides higher bid than his own", async () => {
-    let even_higher_frozen_amount = 5e3;
-    await acg20Inst.freeze(accounts[2], even_higher_frozen_amount, artwork1, {from: admin});
+    let prevBidForArt1 = bidForArt1;
+    bidForArt1 = 5e3;
 
-    let user2Balance = await acg20Inst.balanceOf.call(accounts[2]);
-    assert.equal(user2Balance.toNumber(), userInitBalance-even_higher_frozen_amount, "User's balance should be decreased by his highest bid");
+    await acg20Inst.freeze(bidderForArt1, bidForArt1, artwork1, {from: admin});
+    userBalance[bidderForArt1] += prevBidForArt1;
+    userBalance[bidderForArt1] -= bidForArt1;
+
+    let newBalance = await acg20Inst.balanceOf.call(bidderForArt1);
+    assert.equal(newBalance.toNumber(), userBalance[bidderForArt1], "User's balance should be decreased by his highest bid");
   });
   it("User can bid for more than one artwork at the same time", async () => {
-    let frozen_amount_for_artwork2 = 2e3;
-    let even_higher_frozen_amount = 5e3;
-    await acg20Inst.freeze(accounts[2], frozen_amount_for_artwork2, artwork2, {from: admin});
+    bidderForArt2 = bidderForArt1;
+    bidForArt2 = 2e3;
+    await acg20Inst.freeze(bidderForArt2, bidForArt2, artwork2, {from: admin});
+    userBalance[bidderForArt2] -= bidForArt2;
 
-    let user2Balance = await acg20Inst.balanceOf.call(accounts[2]);
-    assert.equal(user2Balance.toNumber(), userInitBalance-even_higher_frozen_amount-frozen_amount_for_artwork2, "User's balance should be further decreased by his bid for another artwork");
+    let newBalance = await acg20Inst.balanceOf.call(bidderForArt2);
+    assert.equal(newBalance.toNumber(), userBalance[bidderForArt2], "User's balance should be further decreased by his bid for another artwork");
   });
   it("During auction, check recorded bid price and bidder", async () => {
-    let bid_artwork1 = 5e3
-    let bid_artwork2 = 2e3;
-
-    let bidder = await acg20Inst.highestBidder.call(artwork1);
-    let bid = await acg20Inst.highestBid.call(artwork1);
+    let recordBidderForArt1 = await acg20Inst.highestBidder.call(artwork1);
+    let recordBidForArt1 = await acg20Inst.highestBid.call(artwork1);
   
-    assert.equal(bidder, accounts[2], "bidder should be user 2");
-    assert.equal(bid.toNumber(), bid_artwork1, "bid should be 5000")
+    assert.equal(recordBidderForArt1, bidderForArt1, "recorded bidder should be user with highest bid");
+    assert.equal(recordBidForArt1.toNumber(), bidForArt1, "recorded bid should be current highest bid")
 
-    bidder = await acg20Inst.highestBidder.call(artwork2);
-    bid = await acg20Inst.highestBid.call(artwork2);
-    assert.equal(bidder, accounts[2], "bidder should be user 2");
-    assert.equal(bid.toNumber(), bid_artwork2, "bid should be 2000")
+    let recordBidderForArt2 = await acg20Inst.highestBidder.call(artwork2);
+    let recordBidForArt2 = await acg20Inst.highestBid.call(artwork2);
+    assert.equal(recordBidderForArt2, bidderForArt2, "recorded bidder should be user with highest bid");
+    assert.equal(recordBidForArt2.toNumber(), bidForArt2, "recorded bid should be current highest bid")
   });
-  it("At the end of auction, buyer's price should match highest bid", async () => {
-    let frozen_amount_for_artwork2 = 2e3;
-    let wrong_price = frozen_amount_for_artwork2+1;
-    await expectThrow(acg20Inst.transfer(accounts[4], wrong_price, artwork2, {from:accounts[2]}), "Exception: transfer value is mismatching with auction price");
+  it("Bidder's payment should match his bid, or an expection would be thrown out", async () => {
+    let wrongPrice = bidForArt2+1;
+    await expectThrow(acg20Inst.transfer(artist, wrongPrice, artwork2, {from:bidderForArt2}), "Exception: transfer value is mismatching with auction price");
   });
-  it("If contract operation throws exception, all change should be rewinded", async () => {
-    let expectedBalance = 3e3;
-    let user2Balance = await acg20Inst.balanceOf.call(accounts[2]);
-    assert.equal(user2Balance.toNumber(), expectedBalance, "User's balance shall not change if an expection is throw from contract");
+  it("With expection, any change will be rewinded, so user's balance keep unchanged", async () => {
+    let balance = await acg20Inst.balanceOf.call(bidderForArt2);
+    assert.equal(balance.toNumber(), userBalance[bidderForArt2], "User's balance shall not change if an expection is throw from contract");
   });
-  it("At the end of auction, user's balance will be reduced by the final price", async () => {
-    let frozen_amount_for_artwork1 = 5e3;
-    let frozen_amount_for_artwork2 = 2e3;
-    let new_frozen_for_artwork2 = 3e3;
-
+  it("After user pay for auction, his balanced should be decreased", async () => {
     // end of the auction
-    await acg20Inst.transfer(accounts[4], frozen_amount_for_artwork2, artwork2, {from:accounts[2]}), "Exception: transfer value is mismatching with auction price";
+    await acg20Inst.transfer(artist, bidForArt2, artwork2, {from:bidderForArt2});
+    userBalance[artist] += bidForArt2;
 
-    // a new auction starts on the same artwork (should not appear), it won't affect previous bidder's balance
-    await acg20Inst.freeze(accounts[3], new_frozen_for_artwork2, artwork2, {from: admin});
+    let balance = await acg20Inst.balanceOf.call(bidderForArt2);
+    assert.equal(balance.toNumber(), userBalance[bidderForArt2], "Payment should come from the frozen part");
 
-    let user2Balance = await acg20Inst.balanceOf.call(accounts[2]);
-    assert.equal(user2Balance.toNumber(), userInitBalance-frozen_amount_for_artwork1-frozen_amount_for_artwork2, "User's balance should be decreased by final auction payment");
+    balance = await acg20Inst.balanceOf.call(artist);
+    assert.equal(balance.toNumber(), userBalance[artist], "Balance of artist should increase after auction");
   });
 });
 
