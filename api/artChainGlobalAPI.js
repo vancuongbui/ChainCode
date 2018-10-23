33aac59dc7080ee9e70ef38d68255171b34d722f
Lin Yang <liny@outlook.com>
2018-10-23 13:16:20 +1100

    API: add more auxilary methods to setup test environment

--- api/artChainGlobalAPI.js
+++ api/artChainGlobalAPI.js
@@ -1,12 +1,12 @@
 const Web3 = require('web3');
-const Prom = require('bluebird');
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
 
 function ACGChainAPI() {
-    let contract20;
-    let contract721;
 
-    let acg20Inst;
-    let acg721Inst;
+    const contract20 = {abiString:"", bytecode:"", instance:""};
+    const contract721 = {abiString:"", bytecode:"", instance:""};
 
     let web3;
     let administrator;
@@ -27,45 +27,140 @@ function ACGChainAPI() {
         accounts = await web3.eth.getAccounts();
         administrator = accounts[0];
         console.log("Connected to RPC server, set administrator to ", administrator, "...");
-        return true;
+        return web3;
     }
 
-    function simple_test() {
-        console.log("API: ACG20 name =", acg20Inst.name());
-        console.log("API: Owner of ACG721 =", acg721Inst.owner());
+    function compile_contract_from_source(contract_folder) {
+        const CONTRACT_ACG20_PATH = path.resolve(contract_folder, 'acg20.sol');
+        const CONTRACT_ACG721_PATH = path.resolve(contract_folder, 'acg721.sol');
+        const LIB_SAFEMATH_PATH = path.resolve(contract_folder, '..', 'helpers', 'SafeMath.sol');
+        const input = {
+            'acg20.sol': fs.readFileSync(CONTRACT_ACG20_PATH, 'utf8'),
+            'acg721.sol': fs.readFileSync(CONTRACT_ACG721_PATH, 'utf8'),
+            'SafeMath.sol': fs.readFileSync(LIB_SAFEMATH_PATH, 'utf8')
+        };
+        compilingResult = solc.compile({sources: input}, 1, (path) => {
+            // Solc doesn't support importing from other folders
+            // so resolve the missing files here
+            if (path == "helpers/SafeMath.sol") {
+                return {contents: fs.readFileSync('./helpers/SafeMath.sol', 'utf8') };
+            } else {
+                return {error: 'File not found'};
+            }
+        });
+        // Output compiling error and warnings.
+        if (compilingResult.errors) {
+            compilingResult.errors.forEach((errInfo) => {
+                console.log(errInfo);
+            });
+        }
+        // Check if both contracts compiled successfully
+        compiledAcg20 = compilingResult.contracts['acg20.sol:ACG20'];
+        compiledAcg721 = compilingResult.contracts['acg721.sol:ACG721'];
+        if (!compiledAcg20 || !compiledAcg721) {
+            console.log("Compiling contract failed, exit ...");
+            return;
+        }
+        contract20.abiString = compiledAcg20.interface;
+        contract20.bytecode = '0x' + compiledAcg20.bytecode;
+        contract721.abiString = compiledAcg721.interface;
+        contract721.bytecode = '0x' + compiledAcg721.bytecode;
     }
 
-    function set_contract(contracts) {
-        contract20 = contracts[0];
-        contract721 = contracts[1];
+    function read_in_compiled_contract_JSON(contract_folder) {
+        const CONTRACT_ACG20_PATH = path.resolve(contract_folder, "ACG20.json");
+        const CONTRACT_ACG20_SRC = fs.readFileSync(CONTRACT_ACG20_PATH, 'utf8');
 
-        contract20.instance = new web3.eth.Contract(JSON.parse(contract20.abiString),contract20.deployed.options.address);
-        contract721.instance = new web3.eth.Contract(JSON.parse(contract721.abiString), contract721.deployed.options.address);
+        const compiledAcg20 = JSON.parse(CONTRACT_ACG20_SRC);
+        contract20.abiString = JSON.stringify(compiledAcg20.abi);
+        contract20.bytecode = compiledAcg20.bytecode;
+
+        const CONTRACT_ACG721_PATH = path.resolve(contract_folder, 'ACG721.json');
+        const CONTRACT_ACG721_SRC = fs.readFileSync(CONTRACT_ACG721_PATH, 'utf8');
+        
+        const compiledAcg721 = JSON.parse(CONTRACT_ACG721_SRC);
+        contract721.abiString = JSON.stringify(compiledAcg721.abi);
+        contract721.bytecode = compiledAcg721.bytecode;
     }
 
-    async function prepare_test_accounts(user_number, prefund_eth) {
+    function retrieve_deployed_contracts(address) {
+        contract20.instance = new web3.eth.Contract(JSON.parse(contract20.abiString), address[0]);
+        contract721.instance = new web3.eth.Contract(JSON.parse(contract721.abiString), address[1]);
+    }
+
+    async function deploy_new_contracts() {
+        contract20.instance = new web3.eth.Contract(JSON.parse(contract20.abiString), null, {
+            data: contract20.bytecode
+        });
+
+        let estimatedGas = await contract20.instance.deploy().estimateGas();
+        console.log("Estimate to use ", estimatedGas, "gas to deploy the contract");
+        await contract20.instance.deploy().send({
+            from: administrator,
+            gas: estimatedGas
+        });
+
+        contract721.instance = new web3.eth.Contract(JSON.parse(contract721.abiString), null, {
+            data: contract721.bytecode
+        });
+        estimatedGas = await contract721.instance.deploy().estimateGas();
+        console.log("Estimate to use ", estimatedGas, "gas to deploy the contract");
+        await contract721.instance.deploy().send({
+            from: administrator,
+            gas: estimatedGas
+        });
+        console.log("Contracts deployed successfully ...\nACG20 is deployed at: ",
+        contract20.instance.options.address,
+        "\nACG721 is deployed at: ", contract721.instance.options.address);
+    }
+
+    async function simple_test_on_environment() {
+        console.log("******** Simple test ********");
+        const name = await contract20.instance.methods.name().call();
+        const owner = await contract721.instance.methods.owner().call();
+        console.log("Simple test: ACG20 name =", name);
+        console.log("Simple test: Owner of ACG721 =", owner);
+    }
+
+    function get_contracts_instrance() {
+        return [contract20.instance, contract721.instance];
+    }
+
+    async function create_new_account_and_top_up(password, value) {
+        // Create a new account on the node
+        const user = await web3.eth.personal.newAccount(password);
+        // Top up some eth for new user
+        await web3.eth.sendTransaction({
+            from: administrator,
+            to: user,
+            value: web3.utils.toWei(value.toString(), "ether")
+        });
+        return user;
+    };
+
+    async function create_batch_accounts_and_top_up(user_number, password, prefund_eth) {
         console.log("API: ******** Add new users for test ********");
         let users = [];
-        // web3.eth.accounts.create
-        // web3.personal.newAccount
         for (let i=0; i<user_number; i++) {
-            users[i] = await web3.eth.personal.newAccount('password');
+            users[i] = await web3.eth.personal.newAccount(password);
             // send some eth to new user
             await web3.eth.sendTransaction({
                 from: administrator,
                 to: users[i],
                 value: web3.utils.toWei(prefund_eth.toString(), "ether")
             });
-
-            const userBalance = await web3.eth.getBalance(users[i]);
-
-            console.log("User ", users[i], "is added, initial balance is ", web3.utils.fromWei(userBalance, "ether"), "ether");
         };
         return users;
     }
 
-    function get_contracts_instrance() {
-        return [contract20.instance, contract721.instance];
+    async function retrieve_batch_accounts_from_node(user_number) {
+        let users = await web3.eth.getAccounts();
+        // First account is supposed to be administrator
+        // Need at least (user_number) more accounts for general users
+        if (users.length < user_number+1) {
+            error("There aren't as many accounts as expected ...");
+        }
+        return users.slice(2, 2+user_number);
     }
 
     async function add_new_user(user_address) {
@@ -73,6 +168,7 @@ function ACGChainAPI() {
         await contract20.instance.methods.mint(user_address, init_token20_balance).send({
             from: administrator
         });
+        return true;
     }
 
     async function post_new_artwork(user_address, artwork_info) {
@@ -87,19 +183,21 @@ function ACGChainAPI() {
 
         // Store 721 Token for user, because we don't know the size of
         // meta data, so need first estimate required gas amount for the transaction
-        const gasValue = await contract721.instance.methods.mintWithMetadata(user_address, artwork_id, metadata).estimateGas({
-            from: administrator
-        });
-        await contract721.instance.methods.mintWithMetadata(user_address, artwork_id, metadata).send({
-            from: administrator,
-            gas: gasValue + 1e2
-        });
-
+        //const gasValue = await contract721.instance.methods.mintWithMetadata(user_address, artwork_id, metadata).estimateGas({
+        //    from: administrator
+        //});
         // Store 20 Token as prize of posting artwork
-        await contract20.instance.methods.mint(user_address, post_artwork_prize).send({
+        const trans_721_mint = contract721.instance.methods.mintWithMetadata(user_address, artwork_id, metadata).send({
+            from: administrator,
+            gas: 1500000
+        });
+        const trans_20_mint = contract20.instance.methods.mint(user_address, post_artwork_prize).send({
             from: administrator
         });
-
+        console.log("Posted a new artwork, start ...");
+        await trans_721_mint;
+        await trans_20_mint;
+        console.log("Posted a new artwork, stop ...");
         return artwork_id;
     }
 
@@ -143,10 +241,15 @@ function ACGChainAPI() {
         // Auxiliary functions:
         // ----------------------------
         connect_to_chain: connect_to_chain,
-        set_contract: set_contract,
+        compile_contract_from_source: compile_contract_from_source,
+        read_in_compiled_contract_JSON: read_in_compiled_contract_JSON,
+        retrieve_deployed_contracts: retrieve_deployed_contracts,
+        deploy_new_contracts: deploy_new_contracts,
         get_contracts_instrance: get_contracts_instrance,
-        simple_test: simple_test,
-        prepare_test_accounts: prepare_test_accounts,
+        simple_test_on_environment: simple_test_on_environment,
+        create_new_account_and_top_up: create_new_account_and_top_up,
+        create_batch_accounts_and_top_up: create_batch_accounts_and_top_up,
+        retrieve_batch_accounts_from_node, retrieve_batch_accounts_from_node,
         // ----------------------------
         // Standard API definition:
         // ----------------------------
--- test/artChainGlobalAPI.test.js
+++ test/artChainGlobalAPI.test.js
@@ -1,44 +1,86 @@
 const assert = require('assert');
 const Prom = require('bluebird');
+const path = require('path');
+const fs = require('fs');
 
-const contract_compile_deploy = require('../scripts/contract_to_chain_web3_1.0.js');
+//const contract_compile_deploy = require('../scripts/contract_to_chain_web3_1.0.js');
 const AcgApi = require('../api/artChainGlobalAPI.js');
 
-const RPC_SERVER = "http://127.0.0.1:7545"
+const RPC_SERVER = "http://127.0.0.1:31000"
 
-let contracts;
-let users;
+let users = [];
+let web3;
 const acgApi = AcgApi();
 let acg20Inst;
 let acg721Inst;
 
+const USE_DEPLOYED_CONTRACTS = true;
+const SIMPLE_TEST_ON_ENVIRONMENT = true;
 const post_artwork_prize = 1e3;
+const CREATE_NEW_ACCOUNTS_FOR_TEST = false;
+const contract_address = [
+    '0x372AcD4b2090221ED2A0FaB80866D6466559d7CC',
+    '0x8D6637cc64B3C87a7D887a25fE284185f64Dbf85'
+]
 
 describe('A simple test suite', async function () {
     before (async function () {
         console.log("Setup environment start ...");
-        contracts = await contract_compile_deploy(RPC_SERVER);
-        console.log("Setup environment finish ...");
+        //contracts = await contract_compile_deploy(RPC_SERVER);
 
-        await acgApi.connect_to_chain(RPC_SERVER);
-        acgApi.set_contract(contracts);
+        // Connect to RPC server
+       web3 = await acgApi.connect_to_chain(RPC_SERVER);
+
+        // Read in the compiled contract JSON file
+        const contract_folder = path.resolve(__dirname, '..', 'build', 'contracts');
+        acgApi.read_in_compiled_contract_JSON(contract_folder);
+
+        if (USE_DEPLOYED_CONTRACTS) {
+            // Fetch the deployed contracts
+            acgApi.retrieve_deployed_contracts(contract_address);
+        } else {
+            // Deploy new contracts for test
+            acgApi.deploy_new_contracts();
+        }
+
+        if (SIMPLE_TEST_ON_ENVIRONMENT) {
+            acgApi.simple_test_on_environment();
+        }
+
+        // Create test accounts with prefunded eth
+        const user_number = 4;
+        const prefund_eth = 100;
+        if (CREATE_NEW_ACCOUNTS_FOR_TEST) {
+            for (let i=0; i<user_number; i++) {
+                users[i] = await acgApi.create_new_account_and_top_up('password', prefund_eth);
+            }    
+        } else {
+            users = await acgApi.retrieve_batch_accounts_from_node(user_number);
+        }
+        // Print out initial balance of users
+        for (let i=0; i<user_number; i++) {
+            const userBalance = await web3.eth.getBalance(users[i]);
+            console.log("User ", users[i], "is added, initial balance is ", web3.utils.fromWei(userBalance, "ether"), "ether");
+        }
+
+        console.log("Setup environment finish ...");
         
         [acg20Inst, acg721Inst] = acgApi.get_contracts_instrance();
 
-        // Create test accounts with prefunded eth
-        const user_number = 3;
-        const prefund_eth = 100;
-        users = await acgApi.prepare_test_accounts(user_number, prefund_eth);
     });
 
     it('Test API: add_new_user', async () => {
-        await acgApi.add_new_user(users[0]);
+        const new_user_added = await acgApi.add_new_user(users[0]);
+        assert.ok(new_user_added, "Adding new user succeeded");
 
+        // New user's balance is expected to be zero
         const expected_acg20_balance = 0;
         const expected_acg721_balance = 0;
 
-        const acg20_balance = await acg20Inst.methods.balanceOf(users[0]).call();
-        const acg721_balance = await acg721Inst.methods.balanceOf(users[0]).call();
+        const trans_acg20_balance = acg20Inst.methods.balanceOf(users[0]).call();
+        const trans_acg721_balance = acg721Inst.methods.balanceOf(users[0]).call();
+        const acg20_balance = await trans_acg20_balance;
+        const acg721_balance = await trans_acg721_balance;
 
         assert.equal(expected_acg20_balance, Number(acg20_balance),
         "New user's balance of ACG20 token should be zero");
@@ -46,7 +88,7 @@ describe('A simple test suite', async function () {
         "New user's balance of ACG721 token should be zero");
     });
 
-    it('Test API: post_new_artwork', async () => {
+    it('Test API: post_new_artwork', async function (done) {
         const artist = users[1];
         const artwork = {
             "type":"paint",
@@ -57,8 +99,12 @@ describe('A simple test suite', async function () {
         };
 
         // Obtain chain status
-        const acg20_balance_before = await acg20Inst.methods.balanceOf(artist).call();
-        const acg721_balance_before = await acg721Inst.methods.balanceOf(artist).call();
+        const trans_acg20_balance_before = acg20Inst.methods.balanceOf(artist).call();
+        const trans_acg721_balance_before = acg721Inst.methods.balanceOf(artist).call();
+        const acg20_balance_before = await trans_acg20_balance_before;
+        const acg721_balance_before = await trans_acg721_balance_before;
+
+        console.log("Get balance before posting new artworks");
 
         // Post a new artwork, it will:
         // - Add a new 721 token for user
@@ -66,8 +112,10 @@ describe('A simple test suite', async function () {
         const artwork_id = await acgApi.post_new_artwork(artist, artwork);
 
         // Obtain chain status after posting the new artwork
-        const acg20_balance_after = await acg20Inst.methods.balanceOf(artist).call();
-        const acg721_balance_after = await acg721Inst.methods.balanceOf(artist).call();
+        const trans_acg20_balance_after = acg20Inst.methods.balanceOf(artist).call();
+        const trans_acg721_balance_after = acg721Inst.methods.balanceOf(artist).call();
+        const acg20_balance_after = await trans_acg20_balance_after;
+        const acg721_balance_after = await trans_acg721_balance_after;
 
         // Check the change of user's balance
         assert.equal(
